29bb0804e2cb4cd9cd5d59cab07400f11277eb47
KazuyaFki <complete501xx@yahoo.co.jp>
2020-03-16 18:19:59 +0900

    Navigation scrollの導入

--- resources/js/script.js
+++ resources/js/script.js
@@ -13,8 +13,30 @@ $(document).ready(function () {
 
   });
 
+/* Scroll on buttons */
   
+  $('.js--scroll-to-plans').click(function () {
+    $('html, body').animate({ scrollTop: $('.js--section-plans').offset().top }, 1000);
+  });
   
+  $('.js--scroll-to-start').click(function () {
+    $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 1000);
+  });
+  
+/* Navigation scroll */
+  
+  $('a[href*="#"]').on('click', function (e) {
+    e.preventDefault()
+
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top,
+      },
+      1000,
+      'linear'
+    )
+  });
+
   
 
 
